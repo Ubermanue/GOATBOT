c823d71b685e2fdee456231e3609a36b37065f07
Eroiix-xyz <asmitadk31@gmail.com>
2024-07-18 17:18:07 +0545

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ ♕︎ | QueenBot V2 ]";
+const doNotDelete = "[ ASMIT ADK ]";
 /**
 * @author NTKhang
 * @author: do not delete it
